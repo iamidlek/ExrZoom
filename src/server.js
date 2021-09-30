62c07a8d2e9752f1298dcb92d2ad0b0166acfa9a
iamidlek <iamidlek@naver.com>
2021-09-30 15:36:42 +0900

    메세지 주면 전체 소켓에 보내기

--- src/server.js
+++ src/server.js
@@ -1,30 +1,36 @@
-import http from "http"
-import WebSocket from "ws"
-import express from "express"
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
-
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 // express를 이용해 만든 http 서버
-const server = http.createServr(app);
+const server = http.createServer(app);
 // http서버 위에 웹소켓 서버
-const wss = new WebSocket.Server({ server })
+
+const wss = new WebSocket.Server({ server });
+
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
+}
+
+const sockets = [];
 
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to Browser ✅");
-  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("close", onSocketClose);
   socket.on("message", (message) => {
-    console.log(message);
+    sockets.forEach((aSocket) => aSocket.send(message));
   });
-  socket.send("hello!!!");
 });
 
-server.listen(3000, handleListen)
+server.listen(3000, handleListen);
